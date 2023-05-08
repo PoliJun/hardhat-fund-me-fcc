d2b97d921910e7a7e6d84feca4089f138bdd8828
PoliJun <rivenloving@gmail.com>
2023-05-08 13:58:33 +0800

    added helper-hardhat-config and exports networkConfig

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -3,6 +3,7 @@
 // calling of main function
 
 const { network } = require("hardhat")
+const { networkConfig } = require("../helper-hardhat-config")
 
 // function deployFunc(hre) {
 //     console.log("hi")
@@ -24,7 +25,20 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
 
-    
+    // if chainId is X use address Y
+    // if chainId is Z use address A
+
+    const ethUsdPriceFeedAddress=networkConfig[chainId]["ethUsdPriceFeed"]
+
+    // if the contract does not exist, we deploy a minimal version of it for our local testing
+
     // well what happens when we want to  change the chains?
     // when going for localhost or hardhat networks we want to use a mock
+    const fundme = await deployer("FundMe", {
+        from: deployer,
+        args: [
+            /**addresses? */
+        ], // put price feed address
+        log: true,
+    })
 }
